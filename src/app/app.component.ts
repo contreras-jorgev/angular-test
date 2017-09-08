318a8f22d7f29d8f57049e9a8af6f6327a4e65a5
contreras.jorgev <contreras.jorgev@gmail.com>
2017-09-08 15:41:26 -0500

    Add a click handler to expose the selected hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,8 +22,6 @@ const HEROES: Hero[] = [
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-
-
   styles: [ `
     .selected {
       background-color: #CFD8DC !important;
@@ -65,37 +63,37 @@ const HEROES: Hero[] = [
       top: -4px;
       height: 1.8em;
       margin-right: .8em;
-      border-radius: 4px 0 0 4px;  
+      border-radius: 4px 0 0 4px;
     }
     `
   ],
-
-
   template: `
   <h1>{{title}}</h1>
 
 <h2>My Heroes</h2>
 
 <ul class="heroes">
-  <li *ngFor="let hero of heroes">
+  <li *ngFor="let hero of heroes"
+    (click)="onSelect(hero)" >
     <span class="badge">{{hero.id}}</span> {{hero.name}}
   </li>
 </ul>
 
-
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
+<div *ngIf="selectedHero">
+  <h2>{{selectedHero.name}} details!</h2>
+  <div><label>id: </label>{{selectedHero.id}}</div>
   <div>
     <label>name: </label>
-    <input [(ngModel)]="hero.name" placeholder="name">
+    <input [(ngModel)]="selectedHero.name" placeholder="name">
   </div>
+</div>
   `
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero : Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  heroes = HEROES
+  selectedHero: Hero;
+  heroes = HEROES;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
