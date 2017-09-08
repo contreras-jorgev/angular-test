b69804d1316426c7d3b3e883cf1f97f36ecf032b
contreras.jorgev <contreras.jorgev@gmail.com>
2017-09-08 16:45:51 -0500

    The hero service makes a Promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -82,6 +82,6 @@ export class AppComponent implements OnInit {
     this.selectedHero = hero;
   }
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 }
